40154b9f30888e9f50c0a858ad87b1925b5e81e9
Selvaganapathy Kaliamurthy <selvab2v@gmail.com>
2012-10-22 11:57:48 +0530

    updating major bug fixes

--- assests/app.js
+++ assests/app.js
@@ -11,6 +11,11 @@
  
 **/
 
+/***removes the click delay of 300ms***/
+(function($){$.fn.touchMe=function(handler){return $(this).each(function(){$.FastButton($(this)[0],handler);});};$.FastButton=function(element,handler){var startX,startY;var reset=function(){$(element).unbind('touchend');$("body").unbind('touchmove.fastClick');};var onClick=function(event){event.stopPropagation();reset();handler.call(this,event);if(event.type==='touchend'){$.clickbuster.preventGhostClick(startX,startY);}};var onTouchMove=function(event){if(Math.abs(event.originalEvent.touches[0].clientX-startX)>10||Math.abs(event.originalEvent.touches[0].clientY-startY)>10){reset();}};var onTouchStart=function(event){event.stopPropagation();$(element).bind('touchend',onClick);$("body").bind('touchmove.fastClick',onTouchMove);startX=event.originalEvent.touches[0].clientX;startY=event.originalEvent.touches[0].clientY;};$(element).bind({touchstart:onTouchStart,click:onClick});};$.clickbuster={coordinates:[],preventGhostClick:function(x,y){$.clickbuster.coordinates.push(x,y);window.setTimeout($.clickbuster.pop,2500);},pop:function(){$.clickbuster.coordinates.splice(0,2);},onClick:function(event){var x,y,i;for(i=0;i<$.clickbuster.coordinates.length;i+=2){x=$.clickbuster.coordinates[i];y=$.clickbuster.coordinates[i+1];if(Math.abs(event.clientX-x)<25&&Math.abs(event.clientY-y)<25){event.stopPropagation();event.preventDefault();}}}};$(function(){document.addEventListener('click',$.clickbuster.onClick,true);});}(jQuery));
+
+
+
 $(function(){
 	
 	var detectStorage=localStorage.getItem('invoked');
@@ -20,8 +25,7 @@ $(function(){
 		setTimeout(closeMenu,5000);	
 	}
 
-$('#invokeMenu').on("touchstart",openMenu);
-
+$('#invokeMenu').touchMe(openMenu);
 });
 
 function openMenu(){
